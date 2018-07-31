dedf946a9e1fd7131f5bc7da3edf7acdb4de4cbf
Yh-desktop <877617594@qq.com>
2018-07-31 11:49:12 +0800

    Initial commit

---
+++ apps/users/models.py
@@ -0,0 +1,50 @@
+# coding=utf-8
+from __future__ import unicode_literals
+from datetime import datetime
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class UserProfile(AbstractUser):
+    nickname = models.CharField(max_length=50, verbose_name=u"昵称", default="")
+    birday = models.DateField(verbose_name=u"生日", null=True, blank=True)
+    gender = models.CharField(max_length=7, choices=(("male", "男"), ("female", "女")), default="female",
+                              verbose_name="性别")
+    address = models.CharField(max_length=100, default="", verbose_name="地址")
+    mobile = models.CharField(max_length=11, null=True, blank=True, verbose_name="电话")
+    image = models.ImageField(upload_to="image/%Y/%m", default="image/default.png", max_length=100)
+
+    class Meta:
+        verbose_name = "用户信息"
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.username
+
+
+class EmailVerifyRecord(models.Model):
+    code = models.CharField(max_length=20, verbose_name="验证码")
+    email = models.EmailField(max_length=50, verbose_name="邮箱")
+    send_type = models.CharField(choices=(("register", "注册"), ("forget", "忘记密码")), max_length=10,
+                                 verbose_name="验证码类型")
+    send_time = models.DateTimeField(default=datetime.now, verbose_name="发送时间")
+
+    class Meta:
+        verbose_name = "邮箱验证码"
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return '{0}({1})'.format(self.code, self.email)
+
+
+class Banner(models.Model):
+    title = models.CharField(max_length=100, verbose_name="标题")
+    image = models.ImageField(upload_to="banner/%Y/%m", verbose_name="轮播图")
+    url = models.URLField(max_length=200, verbose_name="访问地址")
+    index = models.IntegerField(default=100, verbose_name="顺序")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "轮播图"
+        verbose_name_plural = verbose_name
